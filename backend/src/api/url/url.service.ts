78891c734665995a50461461f2d7e7a65359b99f
guanzhou03 <guanzhou03@gmail.com>
2023-01-14 21:42:26 +0800

    add suspicious words

--- backend/src/api/url/url.service.ts
+++ backend/src/api/url/url.service.ts
@@ -25,6 +25,8 @@ export class UrlService {
     }
 
     private generateHash(originalUrl: string): string {
+        const susAdjectives = ["free", "unlimited", "delightful", "beautiful", "alluring", "amazing", "magical", "imported", "fancy", "fresh", "fragrant", "lovely", "irresistable", "outstanding", "superb"]; 
+        const susNouns = ["c0ins", "d1amonds", "w1fe", "bank", "treasur3", "s1ngles", "g3ms", "cr3dits", "m0ney", "inher1tance", "g0ld", "s1lver", "plat1num", "j3wel", "f0rtun3", "priz3", "ROL3X", "PATEk", "gucc1", "s3x"]
         const hash: string = createHash("md5").update(originalUrl).digest("base64");
         const splits: Array<string> = hash.match(/.{1,8}/g);
         const words = ["hello", "world", "abc"];
