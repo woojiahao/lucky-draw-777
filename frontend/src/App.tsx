27e8d07b29cc6c797d499f984ceefbc5cb973cb7
Woo Jia Hao <woojiahao1234@gmail.com>
2023-01-14 15:02:59 +0800

    Implement frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import bingo from './bingo.jpeg';
+import luckyDraw from './lucky-draw.webp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App flex justify-center flex-col my-16 mx-8">
+      <header className="mb-12">
+        <h1>LuCKY DRAw 777</h1>
       </header>
+
+      <div className="flex justify-between items-center">
+        <img src={bingo} alt="Bingo" className="w-[400px]" />
+        <p className="text-[2rem]">INCREAE TRAFFIK TO UR WEBSIGHT USIN HOUR TOOL!</p>
+      </div>
+
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col justify-center">
+          <h2 className="text-[3rem]">URL HERE</h2>
+          <input type="text" name="url" id="url" className="rounded-lg border-solid border-8 border-black p-8 border-y-2" />
+        </div>
+        <img src={luckyDraw} alt="Lucky Draw" className="w-[400px]" />
+      </div>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Indie Flower'],
+      },
+      fontSize: {
+        base: '18px',
+        'mxl': '5rem'
+      }
+    },
   },
   plugins: [],
 }
