be8c5e8e0fbe9a5b27e0713b98d1008bf819b280
guanzhou03 <guanzhou03@gmail.com>
2023-01-14 21:56:44 +0800

    update hashed return words

--- backend/src/api/url/url.service.ts
+++ backend/src/api/url/url.service.ts
@@ -29,13 +29,15 @@ export class UrlService {
         const susNouns = ["c0ins", "d1amonds", "w1fe", "bank", "treasur3", "s1ngles", "g3ms", "cr3dits", "m0ney", "inher1tance", "g0ld", "s1lver", "plat1num", "j3wel", "f0rtun3", "priz3", "ROL3X", "PATEk", "gucc1", "s3x"]
         const hash: string = createHash("md5").update(originalUrl).digest("base64");
         const splits: Array<string> = hash.match(/.{1,8}/g);
-        const words = ["hello", "world", "abc"];
-        return splits
+        const indexes = splits
             .map((split: string) => (
                 split.split("")
                     .reduce((acc, curr) => acc + curr.charCodeAt(0), 0)
             ))
-            .map((index: number) => words[index % 3])
-            .join("-");
+
+        const susAdjLength = susAdjectives.length;
+        const susNounLength = susNouns.length;
+        const result = [susAdjectives[indexes[0] % susAdjLength], susAdjectives[indexes[1] % susAdjLength], susNouns[indexes[2] % susNounLength]]
+        return result.join("-");
     }
 }
